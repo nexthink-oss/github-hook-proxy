bfc982644d97350f91069c0a170bd9ba883fe4f7
Robin Breathe <robin.breathe@nexthink.com>
2025-03-21 22:30:54 +0100

    chore: bump deps and lightly refactor internals

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/nexthink-oss/github-hook-proxy/cmd"
 )
 
+var (
+	version = "snapshot"
+	commit  = "unknown"
+	date    = "unknown"
+)
+
 func main() {
-	cmd.Execute()
+	cmd := cmd.New()
+	cmd.Version = fmt.Sprintf("%s-%s (built %s)", version, commit, date)
+	if err := cmd.Execute(); err != nil {
+		os.Exit(1)
+	}
 }
